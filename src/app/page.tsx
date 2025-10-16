16d4dde9ee7efbae5394b34784b4a2f39a36d6f8
TrashedPanda2 <therealtrashedpanda@gmail.com>
2025-10-16 19:25:47 -0400

    added import and fixed bug with background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState, useRef, useCallback, useMemo } from "react";
 import dynamic from 'next/dynamic';
+import { AnimatedBackground } from 'animated-backgrounds';
 import { Analytics } from "@vercel/analytics/next"
 
 const useDebounce = (value: string, delay: number) => {
